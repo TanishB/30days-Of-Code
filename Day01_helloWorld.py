48c5657c5656aad86e6e8d45547d3f493aefa1ab
Tanish <tanish.1557@gmail.com>
2019-06-13 06:33:57 +0530

    helloWorld

---
+++ Day01_helloWorld.py
@@ -0,0 +1,9 @@
+# takingInput
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# printing the contents of input_string
+
+print(input_string)
