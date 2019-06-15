16132576525a22e3bc2a260ec656cdd6be1101a6
Tanish <tanish.1557@gmail.com>
2019-06-15 17:33:27 +0530

    dataTypes

---
+++ Day01_dataTypes.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Reading int, float and string
+intNumber = int(input())
+floatNumber = float(input())
+string = input()
+
+# Print the sum of both integer variables on a new line.
+print(intNumber + i)
+# Print the sum of the double variables on a new line.
+print(floatNumber + i)
+# Concatenate and print the String variables on a new line
+print('HackerRank' , string)
+
